b13a84dc779d13c9725a954fc7c014f8a1c886a2
vedvund <vedvund@gmail.com>
2022-05-25 22:37:37 +0530

    updated email..

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 app_name = "website"
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.HomePage.as_view(), name="index"),
 ]
--- website/views.py
+++ website/views.py
@@ -1,19 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
 from .models import *
 from django.contrib.auth.models import User
+from django.views import View
+from django.core.mail import send_mail
+from django.conf import settings
+from datetime import datetime
+from django.contrib import messages
+from django.template.loader import get_template
 
 
-# Create your views here.
-def index(request):
-    user = User.objects.get(username='vedvund')
-    personal_info = PersonalInfo.objects.get()
-    skill_types = SkillType.objects.all()
-    project_details = ProjectDetails.objects.all()
-    skills = Skill.objects.all().order_by('skill_name').order_by('skill_type')
-    return render(request, 'website/index.html', {
-        'user': user,
-        'personal_info': personal_info,
-        'skill_types': skill_types,
-        'skills': skills,
-        'project_details': project_details,
-    })
+class HomePage(View):
+    context = {
+        'user': User.objects.get(username='vedvund'),
+        'personal_info': PersonalInfo.objects.get(),
+        'skill_types': SkillType.objects.all(),
+        'skills': Skill.objects.all().order_by('skill_name').order_by('skill_type'),
+        'project_details': ProjectDetails.objects.all(),
+    }
+
+    @staticmethod
+    def send_user_copy(contact_form):
+        # Auto-Generate subject and message
+        subject = f"Copy of your message to Vedavyas Vundyala"
+
+        with open(str(settings.BASE_DIR) + "/templates/website/contactTemplate.txt") as f:
+            mail_template = f.read()
+
+        message = mail_template
+        message = message.replace('[NAME]', contact_form['name'])
+        message = message.replace('[PURPOSE]', contact_form['purpose'])
+        message = message.replace('[MESSAGE]', contact_form['message'])
+        message = message.replace('[DATE_TIME]', str(datetime.now()))
+
+        # Send mail
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = [contact_form['email'], 'vedvund@gmail.com']
+        send_mail(subject, message, email_from, recipient_list)
+        return True
+
+    def get(self, request):
+        return render(request, 'website/index.html', self.context)
+
+    def post(self, request):
+        if self.send_user_copy(request.POST):
+            messages.success(request, 'Your Message Was Sent Successful...! Copy has been sent to the entered email id')
+        else:
+            messages.error(request, "Your Message wasn't Sent Successful...! Please Try Again...")
+        return redirect(reverse('website:index') + '#contact')
