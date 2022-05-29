cd0a3d79b032943abbe3102ec39fa99e8b52b89a
vedvund <vedvund@gmail.com>
2022-05-29 18:15:16 +0530

    Updated datetime

--- website/models.py
+++ website/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -41,3 +43,4 @@ class ProjectDetails(models.Model):
     github_link = models.CharField(max_length=100, blank=True, null=True)
     website_link = models.CharField(max_length=100, blank=True, null=True)
     icon_image = models.ImageField(null=True, blank=True)
+    start_date = models.DateTimeField(default=datetime.datetime.now(), blank=True, null=True)
